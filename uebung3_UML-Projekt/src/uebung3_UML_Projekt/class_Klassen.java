a33f3ebc95646a17637cbc01fd51d7b1b8b82c1d
SirDanielQ <SirDanielQ@users.noreply.github.com>
2018-12-07 14:20:05 +0100

    class_Klassen(Unfertig nur erstellt :P)

---
+++ uebung3_UML-Projekt/src/uebung3_UML_Projekt/class_Klassen.java
@@ -0,0 +1,10 @@
+package uebung3_UML_Projekt;
+
+public class class_Klassen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ich werde die Klasse Klassen sein");
+	}
+
+}
