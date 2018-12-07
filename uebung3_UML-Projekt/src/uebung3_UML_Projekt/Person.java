097843e9421478acb83838b5bb793e4afd01eaa9
SirDanielQ <SirDanielQ@users.noreply.github.com>
2018-12-07 16:25:18 +0100

    anzahl geadded
    methoden geadded

--- uebung3_UML-Projekt/src/uebung3_UML_Projekt/Person.java
+++ uebung3_UML-Projekt/src/uebung3_UML_Projekt/Person.java
@@ -1,5 +1,27 @@
 package uebung3_UML_Projekt;
+import java.sql.Date;
 
 public class Person {
+	private long svnr;
+	private String vorname;
+	private String nachname;
+	private Date geburtsdatum;
+	private String email;
+	
+	public long getSvnr() {
+		return svnr;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public Date getGeburtsdatum() {
+		return geburtsdatum;
+	}
 
 }
