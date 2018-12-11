d18cbe76042040c1d96ba9d567b3b8491a5753af
SirDanielQ <SirDanielQ@users.noreply.github.com>
2018-12-11 13:45:59 +0100

    Ergänzungen Stevic
    Klasse Lehrer gefüllt
    Testumgebung (HTL) erschaffen

---
+++ uebung3_UML-Projekt/src/uebung3_UML_Projekt/HTL_main.java
@@ -0,0 +1,13 @@
+/*
+ * 	Testumgebung(Versuchsanstalt) HTL für Schule
+ * 	Hier werden versuche an den Schülern durchgeführt.
+ */
+package uebung3_UML_Projekt;
+
+public class HTL_main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- uebung3_UML-Projekt/src/uebung3_UML_Projekt/Lehrer.java
+++ uebung3_UML-Projekt/src/uebung3_UML_Projekt/Lehrer.java
@@ -1,5 +1,12 @@
 package uebung3_UML_Projekt;
 
 public class Lehrer extends Mitarbeiter{
+	private	String kuerzel;
 
+	public String getKuerzel() {
+		return kuerzel;
+	}
+	public void exportStundenplan() {
+		
+	}
 }
